afba7ff976f255e3280332a22053ede14adacf2b
keldonanatol <keldonanatole@gmail.com>
2022-05-16 10:26:26 -0400

    Create Header.js

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+function Header({loggedIn, handleLoggedInClick}) {
+ return (
+     <button onClick={handleLoggedInClick}>
+         {loggedIn ? "Log Out" : "Log In"}
+     </button>
+ );
+ 
+}
+
+
+export default Header
