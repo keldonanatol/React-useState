11a6e47659b694731bfe409a6365264035e53f61
keldonanatol <keldonanatole@gmail.com>
2022-05-16 10:26:44 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Content from "./Content";
+import Header from "./Header";
+import CoinTossCounter from "./CoinTossCounter";
+import SubscribeForm from "./SubscribeForm";
+import SubcsriberList from "./SubscriberList";
+import SubscriberForm2 from "./SubscriberForm2";
+
 
 function App() {
+  const [subscribers, setSubscribers] = useState([]);
+
+  const createSubscriber = (newSubscriber) =>
+    setSubscribers((currentSubscribers) => [
+      newSubscriber,
+      ...currentSubscribers,
+    ]);
+
+    const deleteSubscriber = (indexToDelete) => 
+    setSubscribers((currentSubscribers) => 
+       currentSubscribers.filter((post, index) => index !== indexToDelete)
+    )
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+         <CoinTossCounter />
+         <SubscribeForm createSubscriber={createSubscriber}/>
+         <SubscriberForm2 createSubscriber={createSubscriber}/>
+         <SubcsriberList
+           subscribers={subscribers}
+           deleteSubscriber={deleteSubscriber}
+         />
+    </>     
+  )
+
+//  const [loggedIn, setLoggedIn] = useState(false);
+//  const toggleLoggedIn = () => setLoggedIn(!loggedIn)
+//  return (
+//  <>
+//    <Header loggedIn={loggedIn} handleLoggedInClick={toggleLoggedIn}/>
+//    <Content loggedIn={loggedIn} text= "I love myself..."/>
+//  </>
+//  )
+
+
+//  const [subscribed, setSubscribed] = useState(false); //variable and set variable
+//  const [subscribedCount, setSubscribedCount] = useState(0);
+//  const [alerts, setAlerts] = useState(false);
+//  console.log("Subscribe Status:", subscribed); 
+//  return (
+//    <section>
+//      <p>Please click to subscribe to my updates!</p>
+//      <p>Subscriber Count: {subscribedCount}</p>
+//      <button onClick={() => { 
+//        setSubscribed(!subscribed); 
+//        setSubscribedCount((currentCount) => currentCount +1); 
+//        setSubscribedCount((currentCount) => currentCount +1);
+//        if (!alerts) setAlerts(true)}}>
+//        {subscribed ? "Unsubscribed" : "Subscribed"}
+//      </button>
+//      <button onClick={() => setAlerts(!alerts)}>
+//        {alerts ? "Stop Email Alerts" : "Get Email Alerts"}
+//      </button>
+//    </section>
+//  )
+
+
+//  const handleSubscribe = () => {
+//  console.log("Subscribing!") // prints in the console
+//    alert("You subscribed!")// notification that you subscribed
+//  };
+//onClick={() => console.log("Subscribe")} or onClick={handleSubscribe}
+//  return (
+//    <section>
+//      <p>Please click to subscribe to my updates!</p>
+//      <button onClick={handleSubscribe}>Subscribe</button>// clicking handleSubscribe function
+//    </section>
+//  )
 }
 
 export default App;
